a20300080d6d8dd5a127e5ba002590926eeadf5b
Michal Wit <michal.wit@paramount.com>
2025-10-08 22:34:00 +0200

    Add script for cleaning up all resources

--- aws/cleanup/all.sh
+++ aws/cleanup/all.sh
@@ -1,4 +1,4 @@
-./cleanup_0_bastions.sh
-./cleanup_1_iam_roles.sh
-./cleanup_2_security_groups.sh
-./cleanup_3_eks_cluster_and_vpc.sh
+source ./cleanup_0_bastions.sh
+source ./cleanup_1_iam_roles.sh
+source ./cleanup_2_security_groups.sh
+source ./cleanup_3_eks_cluster_and_vpc.sh
