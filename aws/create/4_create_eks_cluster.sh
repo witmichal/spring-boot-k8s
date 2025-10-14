4f4758d8606a752a951e04800dfb26088aed28e6
Michal Wit <michal.wit@paramount.com>
2025-10-14 10:11:02 +0200

    Use eksctl to create managed cluster

--- aws/create/4_create_eks_cluster.sh
+++ aws/create/4_create_eks_cluster.sh
@@ -1,6 +1,6 @@
 source /Users/michal.wit/workspace/dev_util_scripts/awscli_env/ec2_functions/find_security_group_id_by_name_tag.sh
+source /Users/michal.wit/workspace/dev_util_scripts/awscli_env/vpc_functions/find_vpc_id_by_name_tag.sh
 
-SECURITY_GROUP_ID=$(find_security_group_id_by_name_tag witm-training-sg-ssh-http-icmp)
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text | xargs echo)
 if [[ $ACCOUNT_ID != "709931167372" ]]; then
@@ -12,33 +12,67 @@ fi
 
 export AWS_REGION=eu-central-1
 
-# create role with trust policy from file
-export IAM_ROLE_FOR_EKS_CLUSTER=AmazonEKSAutoClusterRole
-AWS_ROLE_ARN=$(aws iam get-role --role-name $IAM_ROLE_FOR_EKS_CLUSTER --query "Role.Arn" --output text)
-
-export IAM_ROLE_FOR_EKS_NODE=AmazonEKSAutoNodeRole
-AWS_NODE_ROLE_ARN=$(aws iam get-role --role-name $IAM_ROLE_FOR_EKS_NODE --query "Role.Arn" --output text)
-
 export CLUSTER_NAME=michal-wit-training
-export VPC_CF_STACK_NAME=VpcWithTwoPublicSubnetsForEks
+export VPC_NAME=VpcWithTwoPublicSubnetsForEks-VPC
 
-export SUBNETS=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=${VPC_CF_STACK_NAME}*" | jq -r '.Subnets | map(.SubnetId) | join(" ")' | xargs)
-export RESOURCE_VPC_CONFIG=$(jq -nc '{subnetIds: ($ARGS.positional[0] | split(" ")),securityGroupIds:($ARGS.positional[1] | split(" ")),endpointPublicAccess:true,endpointPrivateAccess:true}' --args "$SUBNETS" --args "$SECURITY_GROUP_ID")
-export COMPUTE_CONFIG=$(jq -cn  --arg one 'Apple' --arg nodeRoleArn "$AWS_NODE_ROLE_ARN" '{enabled: true, nodeRoleArn: $nodeRoleArn, nodePools: ["general-purpose","system"]}')
-echo "compute-config:\t\t$COMPUTE_CONFIG\nresources-vpc-config:\t$RESOURCE_VPC_CONFIG"
+VPC_ID=$(find_vpc_id_by_name_tag $VPC_NAME)
 
-aws eks create-cluster \
-  --region $AWS_REGION \
-  --name $CLUSTER_NAME \
-  --kubernetes-version 1.33 \
-  --role-arn $AWS_ROLE_ARN \
-  --resources-vpc-config=${RESOURCE_VPC_CONFIG} \
-  --compute-config=${COMPUTE_CONFIG} \
-  --kubernetes-network-config '{"elasticLoadBalancing": {"enabled": true}}' \
-  --storage-config '{"blockStorage": {"enabled": true}}' \
-  --access-config '{"authenticationMode": "API"}' \
-  --output json | jq
-  
+PRIVATE_SUBNETS=$(
+aws ec2 describe-subnets \
+    --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:aws:cloudformation:logical-id,Values=PrivateSubnet*"\
+    --query "Subnets[].SubnetId" \
+    --output text | xargs echo
+)
+
+#PRIVATE_SUBNETS_ARRAY=(${(@s: :)PRIVATE_SUBNETS})
+#PRIVATE_SUBNETS_ARRAY=(${(@s: :)PRIVATE_SUBNETS})
+IFS=' ' read -r -a PRIVATE_SUBNETS_ARRAY <<< "$PRIVATE_SUBNETS"
+
+for element in "${PRIVATE_SUBNETS_ARRAY[@]}"
+do
+    echo "$element"
+done
+function find_az_for_subnet() {
+   aws ec2 describe-subnets \
+      --subnet-id $1  \
+      --query 'Subnets[0].AvailabilityZone' \
+      --output text | xargs
+}
+
+PRIVATE_SUBNETS_ARRAY_1_AZ=$(find_az_for_subnet ${PRIVATE_SUBNETS_ARRAY[0]})
+PRIVATE_SUBNETS_ARRAY_2_AZ=$(find_az_for_subnet ${PRIVATE_SUBNETS_ARRAY[1]})
+
+
+echo "$PRIVATE_SUBNETS_ARRAY_1_AZ: { id: ${PRIVATE_SUBNETS_ARRAY[0]} }"
+echo "$PRIVATE_SUBNETS_ARRAY_2_AZ: { id: ${PRIVATE_SUBNETS_ARRAY[1]} }"
+
+INSTANCE_CLASS="m7i-flex.large"
+NODE_GROUP="michal-wit-node-group"
+
+cat <<EOF | eksctl create cluster -f -
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+metadata:
+  name: $CLUSTER_NAME
+  region: $AWS_REGION
+  version: "1.33"
+vpc:
+  subnets:
+    private:
+      $PRIVATE_SUBNETS_ARRAY_1_AZ: { id: ${PRIVATE_SUBNETS_ARRAY[0]} }
+      $PRIVATE_SUBNETS_ARRAY_2_AZ: { id: ${PRIVATE_SUBNETS_ARRAY[1]} }
+iam:
+  withOIDC: true  # Required for IRSA
+managedNodeGroups:
+  - name: $NODE_GROUP
+    instanceType: $INSTANCE_CLASS
+    minSize: 2
+    desiredCapacity: 2
+    subnets:
+      - ${PRIVATE_SUBNETS_ARRAY[0]}
+      - ${PRIVATE_SUBNETS_ARRAY[1]}
+EOF
+
 aws eks wait cluster-active --name $CLUSTER_NAME
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
